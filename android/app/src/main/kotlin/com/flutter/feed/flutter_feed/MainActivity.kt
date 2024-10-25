69c5030765b38a21db8cb9c255a751ecc23c94a4
raju <raju348636@gmail.com>
2024-10-25 09:26:33 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/flutter/feed/flutter_feed/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.flutter.feed.flutter_feed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/feed_source.dart
@@ -0,0 +1,7 @@
+
+const List<String> rssFeeds = [
+  'https://www.bacancytechnology.com/blog/flutter/feed',
+  'https://mobikul.com/category/flutter/feed/',
+  'https://verygood.ventures/blog-categories/flutter',
+  'https://codewithandrea.com/rss.xml',
+];
